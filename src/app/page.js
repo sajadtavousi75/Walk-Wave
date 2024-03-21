63a6dab7702f71e098bbf4745c4ea65b45fc46b8
sajadtavoosi <sajad.tavoosi75@gmail.com>
2024-03-21 20:42:02 +0330

    added tailwind style

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="font-bold">sajad</h1>
+    <h1 className="font-bold text-secondary1 ">sajad</h1>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        primary1:'#fff',
+        primary:'#000',
+        secondary1:'#008080',
+        secondary2:'#A62639',
       },
+      fontFamily:{
+        koho:['koho'],
+        quikr:['quikr']
+      }
     },
   },
   plugins: [],
