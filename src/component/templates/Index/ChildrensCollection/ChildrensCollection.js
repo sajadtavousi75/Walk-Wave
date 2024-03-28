ed6faa18fcf1ae8d3844591cb0329baa4eb3266c
sajadtavoosi <sajad.tavoosi75@gmail.com>
2024-03-28 11:38:39 +0330

    complated slider in home page

---
+++ src/component/templates/Index/ChildrensCollection/ChildrensCollection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChildrensCollection() {
+  return (
+    <div className='ChildrensCollection'>
+        
+    </div>
+  )
+}
--- src/component/templates/Index/Slider/SliderHeader.js
+++ src/component/templates/Index/Slider/SliderHeader.js
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState } from "react";
+import styles from './SliderHeader.module.css'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -30,7 +31,7 @@ export default function SliderHeader() {
       rewind={true}
       // navigation={true}
       loop={true}
-      autoplay={{ delay: 2000 }}
+      autoplay={{ delay: 1800 }}
       modules={[Navigation, Autoplay]}
       mousewheel={{enabled: false}} 
       onSlideChange={handelSlide}
@@ -44,63 +45,14 @@ export default function SliderHeader() {
         />
         <img
         className="w-[100%] sm:hidden"
-          src="./images/slider/Frame 59.png"
+          src="./images/slider/1.png"
           alt="Slide"
         />
-        {changeSlide && (
-          <>
-          <motion.div
-        initial={{ opacity: 1, x: 0  }}
-        animate={{ opacity: 1, x: '-70%' }}
-        transition={{ type: 'tween',ease: 'linear', stiffness: 100, duration:4, repeat: Infinity ,loop: Infinity,delay:1 }}
-        style={{ position: 'absolute', bottom: '-20px', left: 0, right: 0 }}
-        >
-        <h1 className=" text-[55px] font-kohob text-primary1 w-[100%]  drop-shadow-xl">Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1</h1>
-        </motion.div>
-        <motion.div
-        initial={{ opacity: 1, x: '100%'}}
-        animate={{ opacity: 1, x: '30%'}}
-        transition={{ type: 'tween',ease: 'linear', stiffness: 100, duration:4, repeat: Infinity ,loop: Infinity,delay:1 }}
-        style={{ position: 'absolute', bottom: '-20px', left: 0, right: 0 }}
-        >
-        <h1 className=" text-[55px] font-kohob text-primary1 w-[100%] drop-shadow-xl">Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1</h1>
-        </motion.div>
-          </>
-        )}
-        
-      </SwiperSlide>
-      <SwiperSlide>
-        <img
-        className="hidden sm:block w-[100%] xl:h-[550px]"
-          src="./images/slider/slide1.png"
-          alt="Slide"
-        />
-        <img
-        className="w-[100%] sm:hidden"
-          src="./images/slider/Frame 59.png"
-          alt="Slide"
-        />
-        {changeSlide && (
-          <>
-          <motion.div
-        initial={{ opacity: 1, x: 0  }}
-        animate={{ opacity: 1, x: '-70%' }}
-        transition={{ type: 'tween',ease: 'linear', stiffness: 100, duration:4, repeat: Infinity ,loop: Infinity,delay:1 }}
-        style={{ position: 'absolute', bottom: '-20px', left: 0, right: 0 }}
-        >
-        <h1 className=" text-[55px] font-kohob text-primary1 w-[100%]  drop-shadow-xl">sajad sajad <span className="m-2"></span> sajad sajad <span className="m-2"></span> sajad sajad <span className="m-2"></span> Nike Air Max 1</h1>
-        </motion.div>
-        <motion.div
-        initial={{ opacity: 1, x: '100%'}}
-        animate={{ opacity: 1, x: '30%'}}
-        transition={{ type: 'tween',ease: 'linear', stiffness: 100, duration:4, repeat: Infinity ,loop: Infinity,delay:1 }}
-        style={{ position: 'absolute', bottom: '-20px', left: 0, right: 0 }}
-        >
-        <h1 className=" text-[55px] font-kohob text-primary1 w-[100%] drop-shadow-xl">sajad sajad <span className="m-2"></span> sajad sajad <span className="m-2"></span> sajad sajad <span className="m-2"></span> Nike Air Max 1</h1>
-        </motion.div>
-          </>
-        )}
-        
+        <div className=" hidden w-[30px] h-[100%] xl:flex flex-col items-center gap-2 font-kohol text-[23px] text-primary1 absolute  top-0 left-48 z-10">
+          <h1 className={styles.animate}>
+            N <br />I <br />K <br /> E  <br /> <br /> A <br /> I <br /> R <br /> M <br /> A <br /> X  <br /> D <br />R <br /> I <br /> F <br /> T
+          </h1>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
         <img
@@ -110,26 +62,31 @@ export default function SliderHeader() {
         />
         <img
         className="w-[100%] sm:hidden"
-          src="./images/slider/Frame 59.png"
+          src="./images/slider/2.png"
           alt="Slide"
         />
-        
-        <motion.div
-        initial={{ opacity: 1, x: 0  }}
-        animate={{ opacity: 1, x: '-70%' }}
-        transition={{ type: 'tween',ease: 'linear', stiffness: 100, duration:4, repeat: Infinity ,loop: Infinity,delay:1 }}
-        style={{ position: 'absolute', bottom: '-20px', left: 0, right: 0 }}
-        >
-        <h1 className=" text-[55px] font-kohob text-primary1 w-[100%]  drop-shadow-xl">Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1</h1>
-        </motion.div>
-        <motion.div
-        initial={{ opacity: 1, x: '100%'}}
-        animate={{ opacity: 1, x: '30%'}}
-        transition={{ type: 'tween',ease: 'linear', stiffness: 100, duration:4, repeat: Infinity ,loop: Infinity,delay:1 }}
-        style={{ position: 'absolute', bottom: '-20px', left: 0, right: 0 }}
-        >
-        <h1 className=" text-[55px] font-kohob text-primary1 w-[100%] drop-shadow-xl">Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1 <span className="m-2"></span> Nike Air Max 1</h1>
-        </motion.div>
+        <div className="hidden w-[30px] h-[100%] xl:flex flex-col items-center gap-2 font-kohol text-[31px] text-primary1 absolute  top-0 left-48 z-10">
+          <h1 className={styles.animate}>
+            N <br />I <br />K <br /> E  <br /> <br /> A <br /> I <br /> R <br /> M <br /> A <br /> X <br />1
+          </h1>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+        className="hidden sm:block w-[100%] xl:h-[550px]"
+          src="./images/slider/3.png"
+          alt="Slide"
+        />
+        <img
+        className="w-[100%] sm:hidden"
+          src="./images/slider/4.png"
+          alt="Slide"
+        />
+        <div className="hidden w-[30px] h-[100%] xl:flex flex-col items-center gap-2 font-kohol text-[31px] text-primary1 absolute  top-0 left-48 z-10">
+          <h1 className={styles.animate}>
+            N <br />I <br />K <br /> E  <br /> <br /> D<br /> U <br /> N <br /> K <br /> L <br /> O <br />W
+          </h1>
+        </div>
       </SwiperSlide>
     </Swiper>
   )
