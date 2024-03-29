517c5474831beffa931a51e28638fa5429c035a4
sajadtavoosi <sajad.tavoosi75@gmail.com>
2024-03-29 20:47:22 +0330

    Added banner section in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Navbar from "@/component/modules/Navbar/Navbar";
+import Banner from "@/component/templates/Index/Banner/Banner";
 import ChildrensCollection from "@/component/templates/Index/ChildrensCollection/ChildrensCollection";
 import NewProducts from "@/component/templates/Index/NewProducts/NewProducts";
 import SliderHeader from "@/component/templates/Index/Slider/SliderHeader";
@@ -12,6 +13,7 @@ export default function Home() {
       <SliderHeader />
       <NewProducts />
       <ChildrensCollection />
+      <Banner />
     </>
   );
 }
---
+++ src/component/templates/Index/Banner/Banner.js
@@ -0,0 +1,61 @@
+"use client"
+import React,{useState , useEffect} from 'react'
+import {motion} from 'framer-motion'
+
+export default function Banner() {
+    const [scrolBanner , setScrolBanner] = useState(false)
+
+    useEffect(()=>{
+        const handelScrol=()=>{
+          const section = document.getElementsByClassName('banner')
+          const sectionOffset= section[0].offsetTop
+          console.log(sectionOffset)
+          const scrolPosition= window.pageYOffset + window.innerHeight
+          console.log(scrolPosition)
+          if(scrolPosition > sectionOffset){
+            setScrolBanner(true)
+          }
+        }
+    
+        window.addEventListener('scroll', handelScrol)
+    
+        return ()=>{
+          window.removeEventListener('scroll', handelScrol)
+        }
+      })
+
+
+  return (
+    <div className='banner w-[100%]  lg:h-[400px]  overflow-y-hidden my-12 relative'>
+        {scrolBanner && (
+            <>
+            <motion.div
+        initial={{ opacity: 1, y:-100}}
+        animate={{ opacity: 1, y: 0}}
+        transition={{  stiffness: 100 , duration:2}}
+        className='w-full '
+        >
+        <img className='w-full '  src="./images/banner/banner1.png" alt="" />
+        </motion.div>
+        <motion.div
+        initial={{ opacity: 1, bottom:-80}}
+        animate={{ opacity: 1, bottom:-30 }}
+        transition={{  stiffness: 100 , duration:2 }}
+        className='absolute hidden md:block left-[15%] lg:left-[25%] xl:left-[30%]'
+        >
+        <img className=''  src="/images/banner/banner2.png" alt="" />
+        </motion.div>
+        <motion.div
+        initial={{ opacity: 1, bottom:-80}}
+        animate={{ opacity: 1, bottom:-60 }}
+        transition={{  stiffness: 100 , duration:2 }}
+        className='absolute md:hidden  min-[500px]:left-[20%] '
+        >
+        <img className=''  src="/images/banner/banner3.png" alt="" />
+        </motion.div>
+            </>
+        )}
+        
+    </div>
+  )
+}
